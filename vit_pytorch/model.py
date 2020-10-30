365a870435e5aacbb20bdf09b104648d3f2bc0b0
Luke <luke.melas@gmail.com>
2020-10-30 01:52:45 +0000

    Fixed sizing comments

--- vit_pytorch/model.py
+++ vit_pytorch/model.py
@@ -139,14 +139,14 @@ class ViT(nn.Module):
         """
         b, c, fh, fw = x.shape
         x = self.patch_embedding(x)  # b,d,gh,gw
-        x = x.flatten(2).transpose(1, 2)  # b,gh*gw,c
+        x = x.flatten(2).transpose(1, 2)  # b,gh*gw,d
         if hasattr(self, 'class_token'):
-            x = torch.cat((self.class_token.expand(b, -1, -1), x), dim=1)  # b,gh*gw,c
+            x = torch.cat((self.class_token.expand(b, -1, -1), x), dim=1)  # b,gh*gw+1,d
         if hasattr(self, 'positional_embedding'): 
-            x = self.positional_embedding(x)  # b,gh*gw,c 
-        x = self.transformer(x)  # b,gh*gw,c
+            x = self.positional_embedding(x)  # b,gh*gw+1,d 
+        x = self.transformer(x)  # b,gh*gw+1,d
         if hasattr(self, 'fc'):
-            x = self.norm(x)[:, 0]  # b,c
+            x = self.norm(x)[:, 0]  # b,d
             x = self.fc(x)  # b,num_classes
         return x
 
