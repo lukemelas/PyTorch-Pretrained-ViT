23160ebb725ba5a6deb8bca501ca135ec12cf701
arkel23 <edwinarkelrios.eic08g@nctu.edu.tw>
2021-05-19 13:23:49 +0800

    changed downloaded models to only standalone vits pretrained on imagenet21k and not finetuned

--- download_convert_models.py
+++ download_convert_models.py
@@ -1,6 +1,6 @@
 from pytorch_pretrained_vit import ViT
 
-models_list = ['B_16', 'B_32', 'L_16', 'L_32', 'H_14', 'B_16_imagenet1k', 'B_32_imagenet1k', 'L_16_imagenet1k', 'L_32_imagenet1k']
+models_list = ['B_16', 'B_32', 'L_16', 'L_32', 'H_14']
 for model_name in models_list:
     model = ViT(model_name, pretrained=True, load_repr_layer=True)
 
